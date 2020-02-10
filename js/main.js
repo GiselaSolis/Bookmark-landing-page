3d6a8aa455e4ea11de740d205485e441d744fc49
GiselaSolis <gisela.solis.roman@gmail.com>
2020-02-10 12:02:28 -0600

    open mobile menu function added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
 
-
 var form = document.querySelector("form");
 form.onsubmit = function (e){
     console.log ("submit");
     e.preventDefault();
 };
 
+
+// Mobile menu
+
+var menuOpen = document.getElementById("hamburger");
+var menu = document.getElementById("mobilemenu")
+var menuClose = document.getElementById("hamburger-close");
+
+function openMenu(){
+    console.log("clicking")
+    if (menu.style.display === "none") {
+        menu.style.display = "flex";
+    } 
+    else {
+    menu.style.display = "none";
+    }
+}
+
+function closeMenu(){
+    if (menu.style.display === "flex") {
+        menu.style.display = "none"
+    }
+}
+
+menuOpen.addEventListener("click", openMenu)
+menuClose.addEventListener ("click", closeMenu)
