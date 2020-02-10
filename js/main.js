013039b0dc7d687b75a3cf1569cdd28febb61b1a
GiselaSolis <gisela.solis.roman@gmail.com>
2020-02-10 12:38:17 -0600

    mobile menu function improved

--- js/main.js
+++ js/main.js
@@ -14,19 +14,42 @@ var menuClose = document.getElementById("hamburger-close");
 
 function openMenu(){
     console.log("clicking")
-    if (menu.style.display === "none") {
-        menu.style.display = "flex";
-    } 
-    else {
-    menu.style.display = "none";
-    }
+    menu.classList.add("active")
 }
+//     if (menu.style.display === "none") {
+//         menu.style.display = "flex";
+//     } 
+//     else {
+//     menu.style.display = "none";
+//     }
+// 
 
 function closeMenu(){
-    if (menu.style.display === "flex") {
-        menu.style.display = "none"
-    }
+    menu.classList.remove("active")
+    // if (menu.style.display === "flex") {
+    //     menu.style.display = "none"
+    // }
 }
 
 menuOpen.addEventListener("click", openMenu)
 menuClose.addEventListener ("click", closeMenu)
+
+
+// Features tabs
+var tabbtn = document.getElementsByClassName("tab")
+
+
+
+// function openCity(evt, cityName) {
+//     var i, x, tablinks;
+//     x = document.getElementsByClassName("city");
+//     for (i = 0; i < x.length; i++) {
+//       x[i].style.display = "none";
+//     }
+//     tablinks = document.getElementsByClassName("tablink");
+//     for (i = 0; i < x.length; i++) {
+//       tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+//     }
+//     document.getElementById(cityName).style.display = "block";
+//     evt.currentTarget.className += " w3-red";
+//   }
