beb294e1939c6a392a1411fa1e0ff6766bafd2e6
GiselaSolis <gisela.solis.roman@gmail.com>
2020-02-11 13:09:36 -0600

    tab functionality added

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 
 var form = document.querySelector("form");
+
 form.onsubmit = function (e){
     console.log ("submit");
     e.preventDefault();
 };
 
 
-// Mobile menu
+//-----------Mobile menu
 
 var menuOpen = document.getElementById("hamburger");
 var menu = document.getElementById("mobilemenu")
@@ -21,106 +22,59 @@ function menuAction()
 menuOpen.addEventListener("click", menuAction);
 menuClose.addEventListener ("click", menuAction);
 
-// function classToggler(element, className)
-// {
-//     element.classList.toggle(className);
-// }
 
-// Element.prototype.classToggler = function (className)
-// {
-//     this.classList.toggle(className);
-// };
+//-----------Feature Tabs
 
-// NodeList.prototype.classToggler = HTMLCollection.prototype.classToggler = function (className)
-// {
-//     for (i = 0; i < this.length -1; i ++)
-//     {
-//         if (this[i])
-//         {
-//             this[i].classToggler(className);
-//         }
-//     }
-// }
+var btnTab = document.getElementsByClassName("tab")
+var contTab = document.getElementsByClassName("tab-content")
+var line = document.getElementsByClassName("red-line")
 
-// let elemento = document.querySelector("body");
+function selectTab (){
+  for (let i = 0; i < btnTab.length; i ++){
+    btnTab[i].onclick = function (){
+      changeTab (i);
+    }
+  }
+}
 
-// classToggler(elemento, "active");
+function changeTab (index){
+  for (let i = 0; i < contTab.length; i ++){
+    if (contTab[i].classList.contains("active")){
+      contTab[i].classList.remove("active");
+    }
+    if (btnTab[i].classList.contains("active")){
+      btnTab[i].classList.remove("active")
+    }
+    if (line[i].classList.contains("active")){
+      line[i].classList.remove("active")
+    }
 
-// document.querySelector("body").classToggler("active");
-// document.querySelectorAll("ul").classToggler("active");
+    contTab[index].classList.add("active");
+    btnTab[index].classList.add("active");
+    line[index].classList.add("active");
+  }
+}
 
-// function openMenu(){
-//     console.log("clicking")
-//     menu.classList.add("active")
-// }
-// //     if (menu.style.display === "none") {
-// //         menu.style.display = "flex";
-// //     } 
-// //     else {
-// //     menu.style.display = "none";
-// //     }
-// // 
-
-// function closeMenu(){
-//     menu.classList.remove("active")
-//     // if (menu.style.display === "flex") {
-//     //     menu.style.display = "none"
-//     // }
-// }
+selectTab()
 
 
+//--------------FAQ's
 
+var questions = document.getElementsByClassName("questions")
+var answers = document.getElementsByClassName("answers")
 
-// Features tabs
+function questionList (){
+  for (let i = 0; i < questions.length; i ++){
+    questions[i].onclick = function (){
+      openAnswer (i);
+    }
+  }
+}
 
-// var tabbtn = document.getElementsByClassName("tab")
-// var tab = document.getElementsByClassName("tab-content")
+function openAnswer (index){
+  for (let i = 0; i < answers.length; i ++){
+    answers[index].classList.add("active")
+  }
+}
 
-// 0
-// 1
-// 2
-
-// contents
-// tab[i].onclick = function() { contents[i].display }
-
-// data-target = "tab1"
-
-// data-tab = "tab1"
-
-
-// let buttons = docuemt.querySelectorAll(".tab-button");
-
-// button.onclick = function()
-// {
-//     let current = document.querySelector("[data-tab = '" + this.dataset.target +  "']")
-// }
-
-
-// 0
-// 1
-// 2
-
-
-// function openTab(event, tabName){
-//     for (i = 0; i < tab.length; i ++){
-//         tab[i].classList.remove = "content-active"    
-//     }
-// }
-
-
-// tabbtn.addEventListener("click", openTab)
-
-
-// function openCity(evt, cityName) {
-//     var i, x, tablinks;
-//     x = document.getElementsByClassName("city");
-//     for (i = 0; i < x.length; i++) {
-//       x[i].style.display = "none";
-//     }
-//     tablinks = document.getElementsByClassName("tablink");
-//     for (i = 0; i < x.length; i++) {
-//       tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
-//     }
-//     document.getElementById(cityName).style.display = "block";
-//     evt.currentTarget.className += " w3-red";
-//   }
+questionList()
